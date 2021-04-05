f849cd18907dc2925bbe8fababbf43f96920c508
Andrew Holway <andrew.holway@gmail.com>
2021-04-05 12:08:44 +0000

    working system

---
+++ set-up-images.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+set -euo pipefail
+
+cd $(dirname "$0") && pwd
+
+export B64_TIMESTAMP=$(date '+%N' | base64)
+export VAPP_PUBLIC_KEYS=$(cat ~/.ssh/id_rsa.pub)
+export SCRIPT_DIR=$(dirname "$0")
+export IMAGE_NAME="ubuntu-1804-bionic-lts-${VAPP_PUBLIC_KEYS:40:5}"
+export VAPP_PUBLIC_KEYS=$(cat ~/.ssh/id_rsa.pub)
+export VAPP_TEMPLATE="true"
+export GOVC_BINARY_URL="https://github.com/vmware/govmomi/releases/download/v0.24.0/govc_linux_amd64.gz"
+export GOVC_FILESYSTEM_LOCATION="bin/govc"
+export ENV_SOURCE_FILESYSTEM_LOCATION="govc_env"
+export UBUNTU_SOURCE_URL="https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.ova"
+# Get positional command line arguments
+export CLUSTER_NAME=$1
+export CLUSTER_SIZE=$2
+export CLUSTER_TYPE=$3
+
+# Test if govc is installed. Install it if not.
+if test -f "$GOVC_FILESYSTEM_LOCATION"; then
+    echo "$GOVC_FILESYSTEM_LOCATION exists."
+else
+    echo "$GOVC_FILESYSTEM_LOCATION does not exist. I will try and install it....."
+    #Install govc 
+    curl -L $GOVC_BINARY_URL | gunzip > $GOVC_FILESYSTEM_LOCATION && chmod +x $GOVC_FILESYSTEM_LOCATION
+fi
+
+# Test if I can source env vars
+if test -f "$ENV_SOURCE_FILESYSTEM_LOCATION"; then
+    echo "$ENV_SOURCE_FILESYSTEM_LOCATION exists. Sourcing it..."
+    source $ENV_SOURCE_FILESYSTEM_LOCATION
+else
+    echo "$ENV_SOURCE_FILESYSTEM_LOCATION is not available. exiting."
+    exit 1
+fi
+
+
+# See if there is already an vm template available. If not, then upload it!
+if $(bin/govc find /*/vm | grep -w -q $IMAGE_NAME) 
+    then
+        echo "image \"$IMAGE_NAME\" is already available."
+    else
+        cat lib/vapp-properties-template.json | envsubst > tmp/$B64_TIMESTAMP.json
+        bin/govc import.ova --options=tmp/$B64_TIMESTAMP.json --name="$IMAGE_NAME" $UBUNTU_SOURCE_URL
+fi
+
+# Create virtual machines
+
+list_of_nodes=""
+for i in $(seq 1 $CLUSTER_SIZE)
+    do
+        nodename=$CLUSTER_NAME-$i
+	bin/govc vm.clone -vm $IMAGE_NAME -folder=databases -c=4 -m=8096 -on=false $nodename
+        bin/govc vm.disk.change -vm $nodename -size 10G
+        list_of_nodes="$list_of_nodes $nodename"
+    done
+
+# Turn on new nodes
+bin/govc vm.power -on $list_of_nodes
+
+# wait for ip-addresses
+list_of_ips=""
+for i in $(seq 1 $CLUSTER_SIZE)
+    do
+        nodename=$CLUSTER_NAME-$i
+        echo "getting IP address for node \"$nodename\". This might take a while."
+	ip=$(bin/govc vm.ip -v4 -wait 5m $nodename)
+	list_of_ips="$list_of_ips,$ip"
+    done
+
+echo "$list_of_ips"
+
+s9s cluster --create --cluster-type=postgresql --vendor=postgres --provider-version=11 --nodes="$list_of_ips" --db-admin="ubuntu" --db-admin-passwd="ubuntu" --os-user=ubuntu --cluster-name="$CLUSTER_NAME" --os-key-file="/home/ubuntu/.ssh/id_rsa" --wait
